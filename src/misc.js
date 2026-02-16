f137178527c60b3b0016668eb99ccf2a1957a113
EmilVitus <emilvitus@gmail.com>
2026-02-16 16:43:17 +0100

    update extension name and comment out code in misc.js

--- src/misc.js
+++ src/misc.js
@@ -1,29 +1,29 @@
 
 // ---------------- Wide lectio ---------------- 
 
-document.getElementById("masterContent").style.width = "100vw";
-document.getElementsByClassName("ls-master-container2")[0].style.width = "100vw";
+//document.getElementById("masterContent").style.width = "100vw";
+//document.getElementsByClassName("ls-master-container2")[0].style.width = "100vw";
 
 // Dashbord
 // BUG: The following code affects more elements than intended. See the profile content islands.
-const layoutElms = document.getElementsByClassName("ls-std-island-layout-ltr");
-if (layoutElms.length !== 0) {
-  for (let i = 0; i < layoutElms.length; i++) {
-    layoutElms[i].style.width = "calc(100vw - 15px)";
-
-    const children = layoutElms[i].children;
-    for (let x = 0; x < children.length; x++) {
-      children[x].style.flex = "1";
-    }
-  }
-
+//const layoutElms = document.getElementsByClassName("ls-std-island-layout-ltr");
+//if (layoutElms.length !== 0) {
+//  for (let i = 0; i < layoutElms.length; i++) {
+//    layoutElms[i].style.width = "calc(100vw - 15px)";
+//
+//    const children = layoutElms[i].children;
+//    for (let x = 0; x < children.length; x++) {
+//      children[x].style.flex = "1";
+//    }
+//  }
+//
   // BUG: This isn't the right fix. The element doesn't always exist.
-  if (layoutElms[0].parentElement.children[2]) {
-    layoutElms[0].parentElement.children[2].style.width = "calc(100vw - 25px)"
-  }
-}
+//  if (layoutElms[0].parentElement.children[2]) {
+//    layoutElms[0].parentElement.children[2].style.width = "calc(100vw - 25px)"
+//  }
+//}
 
-document.querySelector("#s_m_searchpanel").style.width = "unset";
+//document.querySelector("#s_m_searchpanel").style.width = "unset";
 
 // ---------------- Master header -----------------
 
